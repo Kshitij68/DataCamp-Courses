4b4a6e11dddc673ebc984cb93e770cfd61ba503d
Kshitij68 <k.mathur68@gmail.com>
2018-12-19 23:31:02 +0530

    Chapter I unsupervised learning

---
+++ Unsupervised Learning in Python/Chapter I - Clustering for dataset exploration.py
@@ -0,0 +1,17 @@
+"""
+Unsupervised learning finds patterns in data
+E.g. clustering customers by their purchases
+Compressing the data using purchase patterns
+
+Supervised vs Unsupervised Learning
+    Supervised learning finds patterns for a prediction task
+    Eg classify tumor as benign or cancerous (labels)
+
+    Unsupervised learning finds patterns in data
+    But without a specific prediction task in hand
+
+Cluster labels for new samples
+    New samples can be assigned to existing clusters
+    K-Means remembers the mean of each cluster (the 'centroids')
+    Finds the nearest centroid to each new sample
+"""
